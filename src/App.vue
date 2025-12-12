e0668282288e9502ff64412d1a9fd7bbd29b0630
Tasleem <tasleembca31@gmail.com>
2025-12-12 12:07:04 +0530

    fresh branch

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,12 @@
 <template>
-  <div>Mouse position is at: {{ x }}, {{ y }}</div>
-  <div v-if="loading">Loading...</div>
-  <div v-else-if="error">{{ error }}</div>
-
-  <ul v-else style="list-style: decimal">
-    <li v-for="post in slicedData" :key="post.id">{{ post.title }}</li>
-  </ul>
-
-  <button @click="fetchData">Reload</button>
+  <div>
+    <h1>App component</h1>
+  </div>
 </template>
 
 <script>
-import { computed } from "vue";
-import useFetch from "./composable/useFetch";
-import useMouse from "./composable/useMouse";
 export default {
   name: "App",
-  setup() {
-    const { data, loading, error, fetchData } = useFetch(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-    const { x, y } = useMouse();
-
-    const slicedData = computed(() => {
-      return data.value.slice(0, 20);
-    });
-
-    return { data, loading, error, fetchData, x, y, slicedData };
-  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { inputFocus, colorChange } from './directives/custom-directives';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap'; // This imports Bootstrap's JavaScript bundle
 // createApp(App).mount('#app')
 
 const app = createApp(App);
-app.directive('focus', inputFocus);
-app.directive('color-change', colorChange);
 
 app.mount('#app');
