491b062b57fd2d6db65cdda56c1bc5e5f351b956
Mohammad Tasleem <mohammad_tasleem@epam.com>
2025-12-25 17:54:26 +0530

    added json.js file

--- api/json.js
+++ api/json.js
@@ -1,8 +1,9 @@
 
 // api/json.js
+const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // your JSON file
+const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
